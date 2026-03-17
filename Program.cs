5146b4d6cdf0927abe5ba8ee79fe14e91f915250
zero_Marty <osisovsky@gmail.com>
2026-03-17 09:21:51 +0100

    popisky metody

--- Program.cs
+++ Program.cs
@@ -15,7 +15,17 @@ using System.Threading;
     19 - P1.1 - Roztočení
     15 - P1.2 - Zvuk
     20 - P1.3 - Zámek
-    12 - P3.0 - dveřní snímač
+    7 - P3.0 - dveřní snímač
+    14 - P3.1 - teplotní snímač
+    8 -> 11 - P1.3 -> 6 - pozice na displeji (zleva doprava - A1 -> A4
+    12 - výstup klávesnice
+
+    znaky(hex adresy):
+    1 - P2.0 - A
+    2 - P2.1 - B
+    3 - P2.2 - C
+    4 - P2.3 - D
+    5 - P2.4 - E
  */
 namespace Mikrovlnkab
 {
@@ -24,9 +34,12 @@ namespace Mikrovlnkab
 
         static void Main(string[] args)
         {
+            while(true)
+            {
 
+            }
         }
-        public void beh() //změnou střídy lze měnit rychlost otáčení
+        public void beh() //změnou střídy lze měnit rychlost otáčení a intenzitu světla
         {
 
         }
