b329c0945c6b82043c992e72e14410b1e45d2e4a
zero_Marty <osisovsky@gmail.com>
2026-03-17 08:33:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ Schéma zapojení
+  Levá - mikrovlnka, pravá - IO karta
+    24 - GND - GND
+    16 - P1.0 - Rozsvícení
+    19 - P1.1 - Roztočení
+    15 - P1.2 - Zvuk
+    20 - P1.4 - Zámek
+ */
+namespace Mikrovlnkab
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
